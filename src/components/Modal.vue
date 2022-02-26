ae04045e994cfabd18d5681d8e1af9b7a1e1887c
bitameshgi <meshgibita@gmail.com>
2022-02-26 20:53:11 +0330

    new changes

---
+++ src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div
+        class="modalDialog"
+        id="exampleModal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
+        <header class="modal-header" id="modalTitle">
+          <slot name="header"> Your date </slot>
+          <button
+            type="button"
+            class="btn-close"
+            @click="close"
+            aria-label="Close modal"
+          >
+            x
+          </button>
+        </header>
+
+        <section class="modal-body" id="modalDescription">
+          <slot name="body">
+            <h3>{{ TaskDate }}</h3>
+          </slot>
+        </section>
+
+        <footer class="modal-footer">
+          <button
+            type="button"
+            class="btn-green"
+            @click="close"
+            aria-label="Close modal"
+          >
+            Close me!
+          </button>
+        </footer>
+      </div>
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  props: ["TaskDate"],
+  name: "Modal",
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,102 +1,145 @@
 <template>
   <div class="container">
     <div class="card mt-3 shadow w-100">
-      <div class="card-header bg-dark">
-   <h2 class="text-center my-5 text-white "> Todo App </h2>
+      <div class="card-body">
+        <div class="row justify-content-center">
+          <div class="col-md-8 col-lg-6 col-sm-12">
+            <div class="TodoStyle mt-3">
+              <h2 class="text-center text-white">Todo App</h2>
+            </div>
+            <div class="d-flex">
+              <input
+                v-model="task"
+                type="text"
+                placeholder="Enter Todo"
+                class="form-control mt-3 input-style"
+              />
+
+              <button
+                @click="AddTodo"
+                class="btn btn-success rounded mt-3 add-button"
+              >
+                ADD TODO
+              </button>
+            </div>
+
+            <datepicker
+              v-model="TaskDate"
+              type="datetime"
+              placeholder="Enter your time"
+              inputClass="input-datepicker"
+              class="form-control mt-3"
+              v-if="Check"
+              :format="customFormatter"
+            ></datepicker>
+            <div class="form-check mt-2 mb-4">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                v-model="Check"
+                id="flexCheckDefault"
+              />
+              <label class="form-check-label text-white" for="flexCheckDefault">
+                set reminder for this todo
+              </label>
+            </div>
+
+            <ul class="task-item p-0">
+              <li
+                class="task-list-item d-flex"
+                v-for="(task, index) in tasks"
+                :key="index"
+                v-on:dblclick="Highlight(index)"
+                :class="{ highlighted: task.isHighlighted }"
+              >
+                <span class="TitleFont">{{ task.title }}</span>
+
+                <button class="Buttons">
+                  <span @click="CloseTask(index)">
+                    <span class="fa fa-close"></span>
+                  </span>
+                </button>
+
+                <button
+                  class="Buttons"
+                  v-if="
+                    task.TaskDate !== null &&
+                    task.TaskDate !== '' &&
+                    task.TaskDate !== undefined
+                  "
+                >
+                  <span @click="ShowModal(task.TaskDate)">
+                    <span class="fa-solid fa-bell"></span>
+                  </span>
+                </button>
+              </li>
+            </ul>
+            <Modal
+              v-show="isModalVisible"
+              @close="closeModal"
+              v-bind:TaskDate="ModalValue"
+            />
+          </div>
+        </div>
       </div>
-  <div class="card-body">
-    <div class="row justify-content-center" >
-      <div class="col-md-8 col-lg-6 col-sm-12" >
-        <div class="d-flex">
-     <input  v-model="task" type="text" placeholder="Enter Todo" class="form-control my-3">
-     <button @click="AddTodo" class="btn btn-success rounded my-3">ADD TODO </button>
-   </div>
-  
- <ul class="task-item p-0">
-
-   <li
-   class="task-list-item d-flex"
-   v-for="(task,index) in tasks" 
-   :key="index" 
-   v-on:dblclick="Highlight(index)"
-   :class="{highlighted: task.isHighlighted}"
-   >
-
-       <span >{{task.title}}</span>
-
-       <span @click="CloseTask(index)">
-             <span class="fa fa-close"></span>
-       </span>
-   </li>
- </ul>
-      </div>
-    </div>
-    </div>
     </div>
   </div>
-
 </template>
 
 <script>
-
+import Modal from "./Modal.vue";
+import Datepicker from "vuejs-datepicker";
 export default {
-
+  name: "App",
+  components: {
+    Modal,
+    Datepicker,
+  },
   data() {
     return {
-      task:'',
+      Check: false,
+      isModalVisible: false,
+      ModalValue: "",
+      task: "",
+      TaskDate: "",
       tasks: [
         {
-        title: "vue-todo is a todo web app where users can type in their todo and also set a reminder.",
-        isHighlighted : false,
+          title:
+            "vue-todo is a todo web app where users can type in their todo and also set a reminder.",
+          isHighlighted: false,
         },
-        {     
+        {
+          TaskDate: "01 Feb 2022",
           title: "https://vuejsexamples.com/",
-         
-          isHighlighted : false,
+          isHighlighted: false,
         },
       ],
     };
   },
   methods: {
     AddTodo() {
-
-      this.tasks.push({
-        title: this.task,
-        
-      });
-      this.task = '';
+      this.tasks.push({ TaskDate: this.TaskDate, title: this.task });
+      this.task = "";
+      this.TaskDate = "";
     },
     CloseTask(index) {
       this.tasks.splice(index, 1);
     },
-   Highlight: function(index){
-      this.tasks[index].isHighlighted=!this.tasks[index].isHighlighted
-
-   }
+    Highlight: function (index) {
+      this.tasks[index].isHighlighted = !this.tasks[index].isHighlighted;
+    },
+    ShowModal(TaskDate) {
+      this.isModalVisible = true;
+      this.ModalValue = TaskDate;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+    customFormatter(newDate) {
+      newDate = Date.parse(newDate);
+      this.TaskDate = new Intl.DateTimeFormat("en").format(newDate);
+      return this.TaskDate;
+    },
   },
 };
 </script>
-<style scoped>
-.form-control:focus {
-  border-color: #ced4da !important;
-  box-shadow: none;
-}
-.task-list-item {
- border: 1px solid #ced4da;
- align-items: center;
- justify-content: space-between;
-padding: 20px;
-margin-bottom: 10px;
-list-style-type: none;
-color: white;
-}
-.card-body{
-  background-color: rgb(7, 31, 7);
-}
-.btn-success{
-  margin-left:5px ;
-}
-.highlighted{
-  border: rgb(255, 238, 0) solid 3px;
-}
-</style>
